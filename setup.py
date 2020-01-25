e60be3e0a9b56d370bc51bc26cff92ba434a2a24
Hannah May <hannahmay921@gmail.com>
2020-01-24 23:22:10 -0500

    Reconfigured into a pip-installable project

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="tesnames",
+    version="1.0.0",
+    author="Hannah May",
+    author_email="hrmay@protonmail.com",
+    description="A simple name generator that generates names using a Markov chain.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hrmay/TESnames",
+    install_requires=requirements,
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+    python_requires='>=3.6',
+)
